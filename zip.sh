c6eeb85fe9513709d2678741bdde9f20d96214a9
Sungpil Han CPT <shan@acp.kr>
2018-12-27 13:17:21 +0900

    udpate: v1.0

--- zip.sh
+++ zip.sh
@@ -1,4 +1,9 @@
 cp index.R $1.R
-zip -FSr -ll releases/$1.zip $1.R lib simrc assets *.Rmd
+zip -FSr -ll releases/$1.zip *.R lib simrc assets *.Rmd PK34.R74/*.OUT PK34.R74/*.CTL Makefile data-raw
 rm $1.R
+rm -rf tests
+mkdir tests
+unzip releases/$1.zip -d tests
+cd tests
+make run
 
