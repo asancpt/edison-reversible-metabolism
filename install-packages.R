5db8cf9c32f881057d58bb6f4ff7a6d6ceb45d14
Sungpil_edison <shan@acp.kr>
2018-12-27 12:42:29 +0900

    update: v001 test

---
+++ install-packages.R
@@ -0,0 +1,4 @@
+if (Sys.info()['sysname'] == 'Linux') { .libPaths('./lib') }
+
+install.packages(c('deSolve', 'wnl'))
+
--- zip.sh
+++ zip.sh
@@ -1,4 +1,4 @@
 cp index.R $1.R
-zip -FSr -ll releases/be-$1.zip $1.R lib simrc assets *.Rmd
+zip -FSr -ll releases/$1.zip $1.R lib simrc assets *.Rmd
 rm $1.R
 
