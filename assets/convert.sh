535b886fe8d111a005a682a9faecd3ae937ffc69
Sungpil Han CPT <shan@acp.kr>
2019-01-04 18:32:56 +0900

    update: rev-meta

--- assets/convert.sh
+++ assets/convert.sh
@@ -1,1 +1,5 @@
-magick -density 300 figures.pdf figures.png
+magick -density 300 model.pdf model.png
+magick -density 300 figure-pk.pdf figure-pk.png
+magick -density 300 figure-pk-fit.pdf figure-pk-fit.png
+magick -density 300 simulation.pdf simulation.png
+
